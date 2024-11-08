7983906e0a2f4bd912a01a7bef244cc4a0c644e9
Sunaljabili <jabilisunal@gmail.com>
2024-11-08 13:59:34 +0400

    done

---
+++ 06-11 Class,Constructor/js/script.js
@@ -0,0 +1,52 @@
+// // 2)Bir class yaradirsiz ve o classin icerisinde plus, minus, multiply, divide functionlari olur.
+// // var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2) resultin neticesi bu yazilisa gore 50+6-30*3/2 mentiqi ile (riyazi prinsibi unudun,vurma bolme onceliyine ehtiyac yoxdu) 39 olmalidi.
+
+
+// class CustomMatch{
+//     constructor(value){
+//         this.value=value;
+//     }
+
+//     plus(num){
+//         this.value+=num;
+//         return this;
+//     }
+//     minus(num){
+//         this.value-=num;
+//         return this;
+//     }
+//     divide(num){
+//         this.value/=num;
+//         return this;
+//     }
+//     multiply(num){
+//         this.value*=num;
+//         return this;
+       
+//     }
+
+// }
+
+// let result =new CustomMatch(50).divide(5);
+// console.log(result);
+
+
+// let pTag = document.getElementById("demo");
+// pTag.classList.add("color");
+// pTag.classList.add("bg")
+// pTag.style.border="2px solid red"
+// console.log(pTag);
+
+// array iteration
+// foreach
+// reduce
+// filter
+// map
+// find
+// every
+// some
+// sort
+
+// const arr = [34,45,56,567,67];
+// const found = arr.find((item)=>item%2==0);
+// console.log(found);
